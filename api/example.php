21b8e9ab2abd8c6c3e1754538f5e02f47bc31107
Ivan Milincic <hello@kreativan.dev>
2026-05-24 23:37:15 +0300

    Sync from project repository

---
+++ api/example.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ApiRouter Example
+ * /api/api-router/example/
+ */
+
+namespace ProcessWire;
+
+return [
+  "status" => "success",
+  "message" => "Hello from API route example!"
+];
